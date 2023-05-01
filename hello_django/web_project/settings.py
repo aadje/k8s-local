c2a646dd25fd50665598502c61c812793c1ce2ab
Arjan van Rijn <arjan.rijn@mediamonks.com>
2023-05-01 16:09:32 +0200

    Add mysql8 init script for django

--- hello_django/web_project/settings.py
+++ hello_django/web_project/settings.py
@@ -75,13 +75,21 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "hello_django",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "cluster.k8slocal.com",
+        "PORT": "3306"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
